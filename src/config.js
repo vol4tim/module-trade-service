5afe3d1c6492e665140adff2909d8be492bd7755
vol4tim <vol4tim@gmail.com>
2017-02-27 12:07:12 +0400

    upd address, abi

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  builder: '0x35cdc08e6451002682b559dc2a8832aad7ab8392',
-  bank: '0x00E56a1d5AD898166bA90BB90D61AC87FbE319cd',
-  logist: '0x00E56a1d5AD898166bA90BB90D61AC87FbE319cd'
+  builder: '0x34b322ad4f4d2e3a0e21aa023cdc8243f2981d80',
+  bank: '0x75fbe7868011251050df4aa49d4c66f50efd8d27',
+  logist: '0x75fbe7868011251050df4aa49d4c66f50efd8d27'
 }
